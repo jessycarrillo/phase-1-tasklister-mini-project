32e649c9b0129fb698f6d2e569a799bbdad42ef9
Jessica Carrillo <jessymamo@gmail.com>
2023-07-26 12:11:59 -0400

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const taskDescription = e.target.querySelector('#new-task-description').value;
+    buildToDo(taskDescription);
+    e.target.reset(); 
+  });
+}); 
+
+function buildToDo(toDo) {
+  const li = document.createElement('li'); 
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent = ' x ';
+  li.textContent = `${toDo} `;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+}
+function deleteToDo(e) {
+  e.target.parentNode.remove()
+  
+
+}
